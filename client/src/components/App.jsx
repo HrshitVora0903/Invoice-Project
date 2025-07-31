addd32916e3c6ca93ee0e2df8342ea26f536c48c
hrsh <hrshpatel25@gmail.com>
2025-07-31 18:17:38 +0530

    delete function added

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,19 @@
 import React  from "react";
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import InvoiceList from "./InvoiceList";
 import InvoiceForm from "./InvoiceForm";
+
 
 function App(){
     return (
-        <InvoiceForm />
+        <Router>
+      <Routes>
+        <Route path="/" element={<InvoiceList />} />
+        <Route path="/new-invoice" element={<InvoiceForm />} />
+        <Route path="/edit-invoice/:id" element={<InvoiceForm />} />
+
+      </Routes>
+    </Router>
     );
 }
 
